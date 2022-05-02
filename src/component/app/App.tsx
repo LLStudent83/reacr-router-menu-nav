985da29a943bd9e24ae766083e981d3f3a1bbb97
Dmitry Prigulnov <prigulnov@mail.ru>
2022-05-02 12:07:57 +0500

    initial commit

---
+++ src/component/app/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+// import {
+//   useState, useCallback,
+// } from 'react';
+// import { nanoid } from 'nanoid';
+import './app.scss';
+
+function App(): JSX.Element {
+  return (
+    <h1>Привет</h1>
+  );
+}
+
+export default App;
